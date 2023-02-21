f71cc651cf7e0e26a06c064e237deee14bd6f744
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-02-21 12:03:44 +0000

    Add Basic Game Object

--- Source.cpp
+++ Source.cpp
@@ -3,8 +3,36 @@
 #include "SFML/Graphics.hpp"
 #include "SFML.h"
 
+class GameObject: public sf::Drawable {
+
+	std::vector<GameObject*> children;
+
+	void draw(sf::RenderTarget& window, sf::RenderStates states) const {
+		onDraw(window, states);
+		for (const auto& child : children) { window.draw(*child); }
+	}
+
+protected: 
+
+	virtual void onDraw(sf::RenderTarget& window, sf::RenderStates states) const {
+		// To be overriden by children objects
+	}
+
+public:
+
+	GameObject() {	}
+
+	void addChild(GameObject* child) {
+		children.push_back(child);
+	}
+};
+
 int main() {
 	std::cout << "Hello World" << std::endl;
+
+	GameObject root;
+	GameObject child;
+	root.addChild(&child);
 
 	sf::RenderWindow Window(sf::VideoMode(800, 600), "Project");
 	while (Window.isOpen()) {
@@ -14,6 +42,7 @@ int main() {
 			Window.close();
 		}
 		Window.clear();
+		Window.draw(root);
 		Window.display();
 	}
 }
