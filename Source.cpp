d5fe109bb6a091d799588ebba38788376047b737
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-02-21 21:12:43 +0000

    Refactor Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -18,48 +18,16 @@
 
 const sf::Color Cream(244, 244, 166);
 const sf::Color Colors[9] = {
-	sf::Color::Red,
-	sf::Color::Blue,
-	sf::Color::Green,
-	sf::Color::Black,
-	sf::Color::Cyan,
-	sf::Color::Magenta,
-	sf::Color::Yellow,
-	sf::Color::White,
-	Cream
+	sf::Color::Red, sf::Color::Blue, sf::Color::Green,
+	sf::Color::Black, sf::Color::Cyan, sf::Color::Magenta,
+	sf::Color::Yellow, sf::Color::White, Cream
 };
 
 Color swatchColor;
 
-Grid* createButtonOptions() {
-	Grid* buttons = Grid::create(sf::Vector2f(8, 75), sf::Vector2f(50, 50), 8, 2, 4, 4);
-	for (int i = 0; i < 16; i++) {
-		buttons->addChild(Button::create(sf::Color::White, 0, 0, 0, 0));
-	}
-	return buttons;
-}
-
-Grid* createDrawingGrid() {
-	Grid* pixels = Grid::create(sf::Vector2f(190, 75), sf::Vector2f(50, 50), 8, 8, 0, 0);
-	for (int i = 0; i < 64; i++) {
-		Button* button = Button::create(sf::Color::White, 0, 0, 0, 0);
-		PaintTarget* paintTarget = PaintTarget::create(&swatchColor, button);
-		button->setCallback(paintTarget);
-		pixels->addChild(button);
-	}
-	return pixels;
-}
-
-Grid* createColorPalette() {
-	Grid* swatches = Grid::create(sf::Vector2f(632, 350), sf::Vector2f(50, 50), 3, 3, 4, 4);
-	for (int i = 0; i < 9; i++) {
-		Button* button = Button::create(Colors[i], 0, 0, 0, 0);
-		ChangeColor* callback = ChangeColor::create(&swatchColor, Colors[i]);
-		button->setCallback(callback);
-		swatches->addChild(button);
-	}
-	return swatches;
-}
+Grid* createButtonOptions();
+Grid* createDrawingGrid();
+Grid* createColorPalette();
 
 int main() {
 	std::cout << "Hello World" << std::endl;
@@ -94,4 +62,34 @@ int main() {
 			app->redraw();
 		}
 	}
+}
+
+Grid* createButtonOptions() {
+	Grid* buttons = Grid::create(sf::Vector2f(8, 75), sf::Vector2f(50, 50), 8, 2, 4, 4);
+	for (int i = 0; i < 16; i++) {
+		buttons->addChild(Button::create(sf::Color::White, 0, 0, 0, 0));
+	}
+	return buttons;
+}
+
+Grid* createDrawingGrid() {
+	Grid* pixels = Grid::create(sf::Vector2f(190, 75), sf::Vector2f(50, 50), 8, 8, 0, 0);
+	for (int i = 0; i < 64; i++) {
+		Button* button = Button::create(sf::Color::White, 0, 0, 0, 0);
+		PaintTarget* paintTarget = PaintTarget::create(&swatchColor, button);
+		button->setCallback(paintTarget);
+		pixels->addChild(button);
+	}
+	return pixels;
+}
+
+Grid* createColorPalette() {
+	Grid* swatches = Grid::create(sf::Vector2f(632, 350), sf::Vector2f(50, 50), 3, 3, 4, 4);
+	for (int i = 0; i < 9; i++) {
+		Button* button = Button::create(Colors[i], 0, 0, 0, 0);
+		ChangeColor* callback = ChangeColor::create(&swatchColor, Colors[i]);
+		button->setCallback(callback);
+		swatches->addChild(button);
+	}
+	return swatches;
 }
