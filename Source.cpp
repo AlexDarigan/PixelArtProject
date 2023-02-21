dc4b0553f7e638b2001962c736d4b59ee0d9a336
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-02-21 11:36:41 +0000

    Create Window

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,19 @@
 #pragma once
 #include <iostream>
+#include "SFML/Graphics.hpp"
+#include "SFML.h"
 
 int main() {
 	std::cout << "Hello World" << std::endl;
+
+	sf::RenderWindow Window(sf::VideoMode(800, 600), "Project");
+	while (Window.isOpen()) {
+		sf::Event event;
+		Window.pollEvent(event);
+		if (event.type == sf::Event::Closed) {
+			Window.close();
+		}
+		Window.clear();
+		Window.display();
+	}
 }
