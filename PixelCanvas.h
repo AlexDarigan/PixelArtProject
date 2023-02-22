93f941e9569e181d4915b94c8d8e7a0e1c6f66fc
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-02-22 13:21:27 +0000

    Remove setColor

--- PixelCanvas.h
+++ PixelCanvas.h
@@ -68,22 +68,3 @@ public:
 
 };
 
-class SetColor : public Callback {
-
-	PixelCanvas* pixelCanvas;
-	sf::Color color;
-
-protected:
-
-	virtual void onCalled() {
-		std::cout << "Changing Color" << std::endl;
-		pixelCanvas->setColor(color);
-	}
-
-public:
-
-	SetColor(PixelCanvas* pixelCanvas, sf::Color color) {
-		this->pixelCanvas = pixelCanvas;
-		this->color = color;
-	}
-};
