296c7213c07add9f00fdf2e7cbade964d9e29b5c
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-02-22 21:03:33 +0000

    Move Text Input To File

--- Source.cpp
+++ Source.cpp
@@ -5,74 +5,15 @@
 #include "GameObject.h"
 #include "Button.h"
 #include "Grid.h"
-#include "Callback.h"
 #include "Color.h"
 #include "App.h"
 #include "PixelCanvas.h"
 #include "SetColor.h"
-#include "SetPixels.h"
 #include "ToolBox.h"
 #include "SetTool.h"
+#include "TextInput.h"
 
-class TextInput : public GameObject {
 
-	sf::RectangleShape shape;
-	sf::Text text;
-	sf::Font font;
-	CollisionShape* collider;
-	bool isFocused = false;
-
-	bool isAlphaNumericTextEntered(sf::Event& event) {
-		return
-			event.type == event.TextEntered &&
-			(isalnum(event.text.unicode) || event.text.unicode == '.');
-	}
-
-	bool isBackSpace(sf::Event& event) {
-		return
-			event.type == event.KeyPressed &&
-			event.key.code == sf::Keyboard::Backspace;
-	}
-
-protected:
-
-	virtual void onDraw(sf::RenderTarget& window, sf::RenderStates states) const { 
-		window.draw(shape);
-		window.draw(text);
-	}
-
-	virtual void onEvent(sf::Event& event) {
-		if(isAlphaNumericTextEntered(event)) {
-			text.setString(text.getString() + char(event.text.unicode));
-		}
-		else if (isBackSpace(event) && text.getString().getSize() > 0) {
-			std::string oldText = text.getString();
-			oldText.pop_back();
-			text.setString(oldText);
-		}
-	}
-
-public:
-
-	TextInput(Size size, Position position, int textSize = 32, const std::string& fontPath = "arial.ttf") {
-		collider = new CollisionShape(this);
-		shape.setFillColor(sf::Color::White);
-		font.loadFromFile(fontPath);
-		text.setFont(font);
-		text.setCharacterSize(textSize);
-		text.setFillColor(sf::Color::Black);
-		setSize(size);
-		setPosition(position);
-	}
-
-	virtual void setSize(Size size) { shape.setSize(size);  }
-	virtual void setPosition(Position position) { 
-		shape.setPosition(position);
-		text.setPosition(position);
-	}
-	virtual Size getSize() { return shape.getSize(); }
-	virtual Position getPosition() { return shape.getPosition(); }
-};
 
 const sf::Color Cream(244, 244, 166);
 const sf::Color Colors[9] = {
